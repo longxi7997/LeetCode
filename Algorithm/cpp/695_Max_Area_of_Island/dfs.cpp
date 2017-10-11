6bc29ad919c44c21910efaef0e1f384738f7893d
longxi7997 <longxi7997@gmail.com>
2017-10-11 20:29:41 +0800

    Max Area of Island--dfs

---
+++ Algorithm/cpp/695_Max_Area_of_Island/dfs.cpp
@@ -0,0 +1,108 @@
+﻿// Source : https://github.com/longxi7997/LeetCode
+// Author : Xi Long
+// Email  : longxi7997@gmail.com
+// Date   : 2017-06-26
+
+/**********************************************************************************
+* Given a non-empty 2D array grid of 0's and 1's, 
+* an island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) 
+*
+* You may assume all four edges of the grid are surrounded by water.
+*
+* Find the maximum area of an island in the given 2D array. (If there is no island, the maximum area is 0.)
+*
+* 
+* Example 1:
+	[[0,0,1,0,0,0,0,1,0,0,0,0,0],
+	[0,0,0,0,0,0,0,1,1,1,0,0,0],
+	[0,1,1,0,1,0,0,0,0,0,0,0,0],
+	[0,1,0,0,1,1,0,0,1,0,1,0,0],
+	[0,1,0,0,1,1,0,0,1,1,1,0,0],
+	[0,0,0,0,0,0,0,0,0,0,1,0,0],
+	[0,0,0,0,0,0,0,1,1,1,0,0,0],
+	[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+* Given the above grid, return 6. Note the answer is not 11, 
+* because the island must be connected 4-directionally.
+*
+*
+* Example 2:
+	[[0,0,0,0,0,0,0,0]]
+
+* Given the above grid, return 0.
+**********************************************************************************/
+
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <string>
+using namespace std;
+
+
+
+
+class Solution
+{
+public:
+
+	int dfs( vector<vector<int>>& grid ,int  i,int j ) {
+
+		if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] == 0) return 0;
+
+		if ( grid[i][j] >0 )
+		{
+			grid[i][j] = 0;
+			return 1 + dfs(grid, i - 1, j) + dfs(grid, i + 1, j) + dfs(grid, i, j - 1) + dfs(grid, i, j + 1);
+		}
+		
+		return 0;
+	}
+
+	
+	int maxAreaOfIsland(vector<vector<int>>& grid) {
+
+
+		/*for (int i = 0 ; i < grid.size() ; i++)
+		{
+			for (int j = 0 ; j < grid[0].size() ; j++)
+			{
+				cout << grid[i][j] << " ";
+			}
+			cout << endl;
+		}*/
+
+
+		int maxArea = 0;
+		for (int i=0 ; i<grid.size() ; i++)
+		{
+			for (int j=0 ; j<grid[0].size() ; j++)
+			{
+				if ( grid[i][j]>0)
+				{
+					maxArea = max(maxArea, dfs(grid, i, j));
+				}
+				
+			}
+		}
+		return maxArea;
+	}
+};
+
+
+int main()
+{
+
+	Solution *s = new Solution();
+
+	vector<vector<int>> temp = {{1, 1, 0, 1, 0, 0, 1, 1}, { 0, 1, 0, 1, 1, 1, 1, 0 }, { 1, 1, 1, 1, 0, 0, 0, 1 }};
+	
+
+	
+	 cout<< s->maxAreaOfIsland(temp);
+	
+
+	system("pause");
+	return 0;
+
+}
--- LeetCodeProject/LeetCodeProject/main.cpp
+++ LeetCodeProject/LeetCodeProject/main.cpp
@@ -4,19 +4,20 @@
 // Date   : 2017-06-26
 
 /**********************************************************************************
-* Given a digit string, return all possible letter combinations that the number could represent.
+* Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + c + d = target? 
+* Find all unique quadruplets in the array which gives the sum of target.
 *
-* A mapping of digit to letters (just like on the telephone buttons) is given below.
+* Note: The solution set must not contain duplicate quadruplets.
 *
-* 1 2 3 
-* 4 5 6
-* 7 8 9
-* * 0 #
+* For example, given array S = [1, 0, -1, 0, -2, 2], and target = 0.
 *
-* Input:Digit string "23"
-*
-* Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
-*
+* 
+* A solution set is:
+* [
+*   [-1,  0, 0, 1],
+*   [-2, -1, 1, 2],
+*   [-2,  0, 0, 2]
+* ]
 *
 **********************************************************************************/
 
@@ -33,78 +34,103 @@ using namespace std;
 
 class Solution
 {
-private:
-	const string dic[10] = { " " , "1" , "abc" , "def" , "ghi" , "jkl", "mno","pqrs", "tuv", "wxyz" };
-
-	
-	void dfs(vector<string> &res , vector<int> &intDigits, string &dig ,   int cur)
-	{
-		if (cur == intDigits.size())
-		{
-			res.push_back(dig);
-			return ;
-		}
-
-		for (int i = 0 ; i < dic[intDigits[cur]].size() ; i++)
-		{
-			dig.push_back( dic[intDigits[cur]][i] );
-			//dig += dic[intDigits[cur]][i];
-			dfs(res, intDigits, dig, cur + 1);
-
-			dig.pop_back();
-		}
-		
-	}
-
 public:
-	vector<string> letterCombinations(string digits) {
-		
-		vector<string> resl;
+	vector<vector<int>> fourSum(vector<int>& nums, int target) {
 
-		if (digits.length() == 0)
+		int len = nums.size();
+
+
+		vector<vector<int>> ret;
+		int mTarget = target;
+
+		if (len < 3)
+			return ret;
+
+		sort(nums.begin(), nums.end());
+
+
+		for (int i = 0 ; i < len - 3 ; i++)
 		{
-			return resl;
+			
+			for (int secondIndex = i + 1 ; secondIndex < len - 2 ; secondIndex++)
+			{
+				int thirdIndex = secondIndex + 1;
+				int fourthIndex = len - 1 ;
+
+				while (thirdIndex < fourthIndex)
+				{
+					int mSum = nums[i] + nums[secondIndex] + nums[thirdIndex] + nums[fourthIndex];
+					if (mSum < mTarget)
+					{
+						thirdIndex++;
+					}
+					else if (mSum > mTarget)
+					{
+						fourthIndex--;
+					}
+					else
+					{
+						ret.push_back({ nums[i] , nums[secondIndex] ,  nums[thirdIndex] , nums[fourthIndex] , }) ;
+						thirdIndex++;
+						fourthIndex--;
+
+						while (thirdIndex < fourthIndex && nums[fourthIndex] == nums[fourthIndex + 1])
+						{
+							thirdIndex--;
+						}
+						while (thirdIndex < fourthIndex && nums[thirdIndex] == nums[thirdIndex - 1])
+						{
+							thirdIndex++;
+						}
+					}
+
+				}
+
+				while ( secondIndex<len - 3 && nums[secondIndex] == nums[secondIndex + 1])
+				{
+					secondIndex++;
+				}
+			}
+
+			while (i < len - 2 && nums[i] == nums[i + 1])
+			{
+				i++;
+			}
 		}
 
-		vector<int> intDigits;
-		/*for each ( char var in digits)
-		{
-			intDigits.push_back( var-'0' );
-		}*/
-		for (int i = 0 ; i < digits.length() ; i++)
-		{
-			intDigits.push_back(digits[i] - '0');
-		}
 
-		string dig="";
-		dfs( resl , intDigits, dig ,  0 );
-
-		return resl;
+		return ret;
 
 	}
 };
 
 
-
-
-int main()
-{
-
-	Solution *s = new Solution();
-	
-
-	string strTemp = "1";
-
-
-	vector<string> resl = s->letterCombinations(strTemp);
-
-	for ( int i=0; i <resl.size() ; i++ )
-	{
-		cout << resl[i] << endl;
-	}
-		
-
-	system("pause");
-	return 0;
-
-}
+//
+//int main()
+//{
+//
+//	Solution *s = new Solution();
+//	
+//	int temp[] = { 1, 0, -1, 0, -2, 2 };
+//	vector<int> ivector(temp, temp + sizeof(temp) / sizeof(temp[0]));
+//
+//	int target = 0;
+//
+//	vector<vector <int>> resl = s->fourSum(ivector, target);
+//
+//	
+//	for (int i = 0 ; i < resl.size() ; i++)
+//	{
+//		for (int j = 0 ; j < resl[i].size() ; j++)
+//		{
+//			cout << resl[i][j] << ' ';
+//		}
+//		cout << endl;
+//
+//	}
+//
+//
+//	system("pause");
+//	return 0;
+//
+//}
